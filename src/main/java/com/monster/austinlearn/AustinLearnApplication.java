0ba048c63cb63c91bf6805d317614525d94ceab4
“root” <873676545@qq.com>
2022-01-08 22:24:13 +0800

    first commit

---
+++ src/main/java/com/monster/austinlearn/AustinLearnApplication.java
@@ -0,0 +1,13 @@
+package com.monster.austinlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AustinLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monster/austinlearn/AustinLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monster.austinlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AustinLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
