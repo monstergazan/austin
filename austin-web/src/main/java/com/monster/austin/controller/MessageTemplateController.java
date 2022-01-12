98dba5294ced3503177ddd9b90785c7f63b6cc7a
“root” <873676545@qq.com>
2022-01-12 21:06:13 +0800

    add ddl for sms

--- austin-web/src/main/java/com/monster/austin/controller/MessageTemplateController.java
+++ austin-web/src/main/java/com/monster/austin/controller/MessageTemplateController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  *
  */
-@RestController
+//@RestController
 //@RequestMapping("/messageTemplate")
 public class MessageTemplateController {
 
